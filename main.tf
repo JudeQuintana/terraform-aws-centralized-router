dd8ff46c26e8cb132362a3360050397e8957693d
Jude Quintana <jude.quintana@gmail.com>
2024-07-14 10:33:59 -0600

    ipv4 secondary cidrs and ipv6 cidrs support

--- main.tf
+++ main.tf
@@ -2,6 +2,31 @@
 * # Transit Gateway Centralized Router
 * - Creates hub and spoke topology from VPCs.
 *
+* `v1.0.2`
+* - New [Dual Stack Networking Trifecta Demo](https://github.com/JudeQuintana/terraform-main/tree/main/dual_stack_networking_trifecta_demo)
+* - Supports auto routing IPv4 secondary cidrs and IPv6 cidrs in addtion to IPv4 network cidrs
+*   - Can blackhole IPv6 cidrs
+*
+* `v1.0.2` example:
+* ```
+* module "centralized_router" {
+*   source  = "JudeQuintana/centralized-router/aws"
+*   version = "1.0.2"
+*
+*   env_prefix       = var.env_prefix
+*   region_az_labels = var.region_az_labels
+*   centralized_router = {
+*     name            = "gambit"
+*     amazon_side_asn = 64512
+*     vpcs            = module.vpcs
+*     blackhole = {
+*       cidrs      = ["172.16.8.0/24"]
+*       ipv6_cidrs = ["2600:1f24:66:c109::/64"]
+*     }
+*   }
+* }
+* ```
+*
 * `v1.0.1`
 * - Now supports VPC attachments for private subnets.
 *   - Uses the subnet id for a subnet tagged with `special = true` from either a private or a public subnet per AZ in Tiered VPC-NG for `v1.0.1`
@@ -18,13 +43,13 @@
 *   centralized_router = {
 *     name            = "bishop"
 *     amazon_side_asn = 64512
-*     blackhole_cidrs = ["172.16.8.0/24"]
 *     vpcs            = module.vpcs
+*     blackhole_cidrs = ["172.16.8.0/24"]
 *   }
 * }
 * ```
 *
-* `v1.0.1`
+* `v1.0.0`
 * - This Transit Gateway Centralized Router module will create a hub spoke and topology from existing Tiered VPCs.
 * - Will use the special public subnet in each AZ when a Tiered VPC is passed to it.
 * - All attachments will be associated and routes propagated to one TGW Route Table.
