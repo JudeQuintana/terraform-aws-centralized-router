3656283739997409f21fc6d1c1e550835615443b
Jude Quintana <jude.quintana@gmail.com>
2026-08-15 19:25:02 -0600

    pull generate routes from tf registry

--- generate_routes.tf
+++ generate_routes.tf
@@ -1,6 +1,7 @@
 # Create routes to other VPC network_cidrs in private and public route tables for each VPC
 module "this_generate_routes_to_other_vpcs" {
-  source = "git@github.com:JudeQuintana/terraform-aws-generate-routes-to-other-vpcs.git?ref=routing-policy"
+  source  = "JudeQuintana/generate-routes-to-other-vpcs/aws"
+  version = "1.1.0"
 
   routing_policy = var.routing_policy
   vpcs           = local.vpcs
